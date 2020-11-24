6af28d0de35dec41c6e58108c73ab6e5ef2ef3a8
Michal Slawinski <michal.slawin@gmail.com>
2020-11-24 16:22:00 +0100

    created pieces selection script

--- Assets/Chess3d/Scripts/PiecesController.cs
+++ Assets/Chess3d/Scripts/PiecesController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PiecesController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Color32 selectColor = new Color32(107, 230, 46, 255);
+    private Color32 originalColor = new Color32(255, 255, 255, 255);
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleSelection();
+    }
+
+    private void HandleSelection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                GameObject gameObject = hit.transform.gameObject;
+                if (gameObject.CompareTag("PlayerControllable"))
+                {
+                    HandlePieceSelection(gameObject);
+                }
+            }
+        }
+    }
+
+    private void HandlePieceSelection(GameObject gameObject)
+    {
+        GameObject baseObj = gameObject.transform.GetChild(0).gameObject;
+
+        Color32 currentColor = baseObj.GetComponent<Renderer>().material.color;
+
+        if (currentColor.Equals(selectColor))
+        {
+            baseObj.GetComponent<Renderer>().material.color = originalColor;
+        }
+        else if (currentColor.Equals(originalColor))
+        {
+            baseObj.GetComponent<Renderer>().material.color = selectColor;
+        }
     }
 }
