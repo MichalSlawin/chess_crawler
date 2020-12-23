1d04268702691a9c0544657492857c9924c3d918
Michal Slawinski <michal.slawin@gmail.com>
2020-12-23 18:59:11 +0100

    fps limit

---
+++ Assets/Chess3d/Scripts/BeforeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+class BeforeScene
+{
+
+    private static int maxFps = 120;
+
+    public static int MaxFps { get => maxFps; set => maxFps = value; }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        Application.targetFrameRate = MaxFps;
+    }
+
+}
